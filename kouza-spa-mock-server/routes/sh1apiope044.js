fa9d47dbc23b356754b353417a051a3bc7508468
movanda <movanda@hotmail.co.jp>
2023-03-14 11:35:43 +0900

    add: 4 case data in the switch statement

--- kouza-spa-mock-server/routes/sh1apiope044.js
+++ kouza-spa-mock-server/routes/sh1apiope044.js
@@ -1,27 +1,33 @@
-const express = require('express');
-const jsonServer = require('../utils/file_server');
+const express = require("express");
+const jsonServer = require("../utils/file_server");
 const router = express.Router();
 const requestMap = new Map();
 
 router
-  .post('', (req, res) => {
-    console.log('sh1apiope044', req.body);
-    switch (req.body.ncoLocation) {
-      case "1":
-        jsonServer.receiveUnencryptData(res, 'sh1apiope044/normal/4-1-7.json');
+  .post("", (req, res) => {
+    console.log("sh1apiope044", req.body);
+    var ncoLocation = req.body.ncoLocation;
+    var shopNoSetted = req.body.shopNoSetted;
+    switch (true) {
+      case ncoLocation === "1":
+        jsonServer.receiveUnencryptData(res, "sh1apiope044/normal/4-1-3.json");
         break;
-      case "2":
-        jsonServer.receiveUnencryptData(res, 'sh1apiope044/normal/4-1-4.json');
+      case ncoLocation === "4":
+        jsonServer.receiveUnencryptData(res, "sh1apiope044/normal/4-1-5.json");
         break;
-    
-        default:
-          jsonServer.receiveUnencryptData(res, 'sh1apiope044/normal/2-1-1.json');
+      case shopNoSetted === "0012":
+        jsonServer.receiveUnencryptData(res, "sh1apiope044/normal/4-1-3.json");
+        break;
+      case ncoLocation === "1" && shopNoSetted === "0012":
+        jsonServer.receiveUnencryptData(res, "sh1apiope044/normal/4-1-3.json");
+        break;
+
+      default:
+        jsonServer.receiveUnencryptData(res, "sh1apiope044/normal/2-1-1.json");
     }
-
-
-
   })
-  .all('', (req, res) => {
+
+  .all("", (req, res) => {
     jsonServer.receiveErrorData(res, 405);
   });
 
