2f2f2f7452dbf098b92cdd7f72cbe977bdca736c
movanda <movanda@hotmail.co.jp>
2023-03-14 11:26:57 +0900

    add: auto-refresh feature and comments for each functionality. Fix: the issue with data exchange between front-end and back-end.

--- kouza-spa/src/pages/operator-status-list/OperatorStatusList.tsx
+++ kouza-spa/src/pages/operator-status-list/OperatorStatusList.tsx
@@ -1,4 +1,6 @@
+// 安装第三方，自己的api，css文件等。顺序依次从第三方到自己。
 import React, { useState } from "react";
+import { useEffect } from "react";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
@@ -26,13 +28,14 @@ import {
 import { SH1APIOPE044RequestBody } from "../../assets/interfaces/api/sh1apiope044";
 import { getApi } from "../../common/service/ApiUtils";
 import { ApiIds } from "../../assets/constants/api-id.constant";
-import { useEffect } from "react";
 
+// 定义List的类型
 interface List {
   code: string;
   label: string;
 }
 
+// 定义Operator的类型
 interface Operator {
   operatorID: string;
   operatorName: string;
@@ -46,6 +49,7 @@ interface Operator {
   businessRoleName: string;
 }
 
+// 定义SearchParams的类型
 interface SearchParams {
   selectedNcoLocation: string;
   selectedRoleNo: string;
@@ -55,8 +59,10 @@ interface SearchParams {
   inputShopNameSetted: string;
 }
 
+// 定义表格中，每一页显示的数据量为10条
 const DEFAULT_PER_PAGE_SIZE = 10;
 
+//
 const DEFAULT_SEARCH_PARAMS: SearchParams = {
   selectedNcoLocation: "",
   selectedRoleNo: "",
@@ -66,6 +72,7 @@ const DEFAULT_SEARCH_PARAMS: SearchParams = {
   inputShopNameSetted: "",
 };
 
+// 定义responseBoday的初期设定
 const responseBody = {
   inStandby: 1,
   inHand: 1,
@@ -86,22 +93,10 @@ const responseBody = {
       tabletName: "新規账号開設新規账号開設账号開設",
       businessRoleName: "新規账号開設",
     },
-    {
-      operatorID: "XXX00002",
-      operatorName: "社員B",
-      necLocation: "新宿",
-      roleNo: "2",
-      roleName: "CSR",
-      operatorStatus: "待機中",
-      passingTime: "1",
-      shopNoSetted: "0012",
-      tabletName: "新規账号開設新規账号開設账号開設",
-      businessRoleName: "新規账号開設",
-    },
   ],
 };
 
-// top style
+// 头部样式设定
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
@@ -114,6 +109,7 @@ const Item = styled(Paper)(({ theme }) => ({
   alignItems: "center",
 }));
 
+// 表格单元样式设定
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.common.black,
@@ -124,6 +120,7 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
   },
 }));
 
+// 表格行的样式设定
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   "&:nth-of-type(odd)": {
     backgroundColor: theme.palette.action.hover,
@@ -140,8 +137,15 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
  * @return {JSX.Element}
  */
 const OperatorStatusList = (): JSX.Element => {
+  // 使用了React中的useState hook来声明了一个名为operator的state变量，并将其初始值设为responseBody。同时，还声明了一个名为setOperator的函数，用于更新operator的值。
   const [operator, setOperator] = useState<any>(responseBody);
+  // 使用了React中的useState hook来声明了一个名为currentPage的state变量，并将其初始值设为1。同时，还声明了一个名为setCurrentPage的函数，用于更新currentPage的值。
   const [currentPage, setCurrentPage] = useState(1);
+  /**
+   * 使用了React中的useState hook来声明了一个名为searchParams的state变量，并将其初始值设为一个对象，该对象的各个属性均为字符串类型。
+   * 同时，还声明了一个名为setSearchParams的函数，用于更新searchParams的值。
+   * 在后续的组件渲染过程中，如果调用了setSearchParams函数更新了searchParams的值，组件会重新渲染，反映新的searchParams的值。
+   */
   const [searchParams, setSearchParams] = useState<SearchParams>({
     selectedNcoLocation: "",
     selectedRoleNo: "",
@@ -150,23 +154,57 @@ const OperatorStatusList = (): JSX.Element => {
     inputShopNoSetted: "",
     inputShopNameSetted: "",
   });
+  /**
+   * 使用了React中的useState hook来声明了一个名为searchDefaultParams的state变量，并将其初始值设为一个对象，该对象的各个属性均为字符串类型。
+   * 同时，还声明了一个名为setSearchDefaultParams的函数，用于更新searchDefaultParams的值。
+   * 该对象的初始值为一个常量DEFAULT_SEARCH_PARAMS，该常量也是一个SearchParams类型的对象，表示搜索条件的默认值。
+   * 在后续的组件渲染过程中，如果调用了setSearchDefaultParams函数更新了searchDefaultParams的值，组件会重新渲染，反映新的searchDefaultParams的值。
+   */
   const [searchDefaultParams, setSearchDefaultParams] = useState<SearchParams>(
     DEFAULT_SEARCH_PARAMS
   );
 
   // pagination
+  // 开始页：（当前页-1）* 一页10条数据  → 减一是因为下标从0开始
   const startIndex = (currentPage - 1) * DEFAULT_PER_PAGE_SIZE;
+  /**
+   * 结束页：
+   * 1. startIndex + DEFAULT_PER_PAGE_SIZE - 1，表示当前页的结束位置，因为数组索引从0开始，所以需要将索引加上DEFAULT_PER_PAGE_SIZE - 1。
+   * 2. operator.operatorList.length - 1，表示数据总数减一，因为数组索引从0开始。
+   *
+   * Math.min函数的作用是取两个值中的较小值。这是为了确保当前页的结束位置不会超过数据总数。
+   * 如果计算出来的结束位置超过了数据总数，那么就取数据总数减一作为当前页的结束位置。这样可以避免数组越界的错误。
+   */
   const endIndex = Math.min(
     startIndex + DEFAULT_PER_PAGE_SIZE - 1,
     operator.operatorList.length - 1
   );
+  /**
+   * slice函数是JavaScript数组的一个方法，用于返回一个从原数组中指定位置开始取出的一段新数组。slice函数取出了从startIndex到endIndex之间的数据，这个数据就是当前页需要显示的数据列表。
+   * 注意：slice函数不会修改原数组，而是返回一个新数组。
+   */
   const currentPageData = operator.operatorList.slice(startIndex, endIndex);
 
+  /**
+   * event表示触发页面变化事件的事件对象，page表示变化后的页码。
+   * 当用户点击页面切换按钮时，handlePageChange函数将传递给它的page参数作为新的页码，通过调用setCurrentPage来更新当前页的值。
+   */
   const handlePageChange = (event: any, page: number) => {
     setCurrentPage(page);
   };
 
   // searchBar
+  /**
+   * event表示触发下拉框变化事件的事件对象，name表示对应的下拉框名称。
+   * setSearchParams是一个React Hook函数，用于更新searchParams的值，进而实现搜索条件的动态更新。
+   * 当用户选择下拉框中的选项时，handleSelectChange函数会将用户选择的选项的value值作为新的搜索条件，通过调用setSearchParams来更新搜索条件。这样，在组件的下次渲染中，就会显示更新后的搜索结果。
+   *
+   * 【...】为ES6的解构语法，将prevSearchParams对象中的属性展开，然后再用新的属性值覆盖旧的属性值，从而实现了只更新指定属性的效果。
+   *
+   * [name]: event.target.value as string是一个对象字面量的语法
+   * name是一个变量，表示下拉框的名称，通过这个变量动态地设置searchParams对象的属性。（如这里的拠点別、権限スキル等）
+   * event.target.value表示用户选择的下拉框选项的值，这个值可能是一个字符串、数字、布尔值等，需要使用as string来将它强制转换为字符串类型，以保证属性值的一致性。（如新宿，日本桥，CSR等）
+   */
   const handleSelectChange = (
     event: SelectChangeEvent<string>,
     name: string
@@ -177,6 +215,11 @@ const OperatorStatusList = (): JSX.Element => {
     }));
   };
 
+  // input
+  /**
+   * event表示触发输入框变化事件的事件对象，name表示对应的输入框名称。
+   * 当用户在输入框中输入文本时，handleInputChange函数会将用户输入的文本作为新的搜索条件，通过调用setSearchParams来更新搜索条件。
+   */
   const handleInputChange = (
     event: React.ChangeEvent<HTMLInputElement>,
     name: string
@@ -190,13 +233,21 @@ const OperatorStatusList = (): JSX.Element => {
   /**
    * renderSelect
    *
-   * @param {List[]} List
-   * @return {JSX.Element}
+   * renderSelect的函数用于渲染一个下拉框。它接受三个参数：
+   * List：一个数组，表示下拉框选项的列表。
+   * Name：一个字符串，表示下拉框的名称，用于在handleSelectChange函数中动态更新对应的搜索条件。
+   * Value：一个字符串，表示下拉框当前选中的值。
+   *
+   * 函数内部首先使用map方法遍历List数组，生成一个包含所有下拉框选项的item数组。
+   * 在遍历的过程中，使用MenuItem组件来生成每一个下拉框选项，并使用code作为选项的值，使用label作为选项的文本。（顶部定义了List的类型）
+   * 接着，在函数的返回值中，使用Select组件来渲染下拉框。value属性指定了下拉框当前选中的值，onChange属性指定了选项变化时要执行的回调函数。
+   * 当下拉框选项发生变化时，onChange回调函数会触发handleSelectChange函数，并将选中的值和下拉框名称作为参数传入，以便动态更新对应的搜索条件。
+   * 最后，函数将生成的下拉框组件返回，供组件进行渲染。
    */
   const renderSelect = (
     List: List[],
-    Value: string,
-    Name: string
+    Name: string,
+    Value: string
   ): JSX.Element => {
     const item = List.map((ele: List, index: number) => {
       return (
@@ -220,7 +271,10 @@ const OperatorStatusList = (): JSX.Element => {
   /**
    * BasicTable
    *
-   * @returns
+   * 返回一个表格，表格的内容来自于一个名为operator的变量，包括6列，分别是“待機中”，“受付中”，“後処理中”，“離席 1”，“離席 2”，“離席 3”。
+   * 每列的数据都是通过在operator变量上访问对应的属性获得的，并使用TableCell组件在表格中显示。
+   * 整个表格使用TableContainer和Paper组件包裹起来，使用sx属性设置了最小宽度为650，以便适应屏幕大小。
+   * 表头使用TableHead和TableRow组件进行设置，包含了6个单元格。表体使用TableBody和TableRow组件进行设置，包含了一个行，也就是上面提到的6个单元格的数据。
    */
   const BasicTable = () => {
     return (
@@ -250,6 +304,7 @@ const OperatorStatusList = (): JSX.Element => {
           </Table>
         </TableContainer>
 
+        {/* 当点击最新化按钮时触动handleRefresh */}
         <Button
           variant="contained"
           style={{
@@ -267,6 +322,13 @@ const OperatorStatusList = (): JSX.Element => {
     );
   };
 
+  /**
+   * 定义了一个异步函数 handleInquery，主要功能是获取搜索条件 searchParams。
+   * 根据这些条件发送请求 getApi(ApiIds.SH1APIOPE044, param) 并获取响应 response，最后将响应数据保存到组件的状态 operator 中，并将当前页数 currentPage 设置为 1。
+   *
+   * 首先从 searchParams 中解构出搜索条件，然后根据这些条件构造请求参数 param。
+   * 接着调用 getApi 函数发送请求，并将响应数据保存到组件的状态 operator 中。最后将当前页数 currentPage 设置为 1，这是因为搜索条件发生变化后需要从第一页重新开始显示数据。
+   */
   const handleInquery = async () => {
     const {
       selectedNcoLocation,
@@ -277,6 +339,7 @@ const OperatorStatusList = (): JSX.Element => {
       inputShopNameSetted,
     } = searchParams;
 
+    // 当最新化里没有数据，或者数据与检索后的数据不一致时，把检索后的数据赋值给最新化。
     if (!searchDefaultParams || searchDefaultParams !== searchParams) {
       setSearchDefaultParams(searchParams);
     }
@@ -297,6 +360,11 @@ const OperatorStatusList = (): JSX.Element => {
     setCurrentPage(1);
   };
 
+  /**
+   * 用于刷新当前查询条件下的操作员数据的函数
+   * 首先获取之前保存的查询条件searchDefaultParams，然后根据这些条件构造一个API请求的参数对象param，并使用该参数调用getApi函数来获取数据。
+   * 最后，它使用setOperator函数将新数据设置为当前操作员数据，实现了刷新操作。
+   */
   const handleRefresh = async () => {
     const {
       selectedNcoLocation,
@@ -323,12 +391,21 @@ const OperatorStatusList = (): JSX.Element => {
   };
 
   /**
-   * pagination
+   * 自动更新
    *
-   * @returns
+   * handleRefresh 函数会被调用，它会请求一些数据并更新组件状态。setInterval 函数会定时调用 handleRefresh 函数，每隔 60 秒刷新一次数据。
+   * 在 useEffect 的返回值中，又包含了一个 clearInterval 函数，用于清除定时器，防止内存泄漏。
+   * 整个 useEffect 函数在组件挂载后只会执行一次，因为第二个参数是一个空数组，这个空数组表示该 useEffect 只在组件挂载时执行一次，不会在组件的重新渲染中重复执行。
    */
+  useEffect(() => {
+    handleRefresh();
 
-  //
+    var timer = setInterval(() => {
+      handleRefresh();
+    }, 60000);
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       {/* top */}
@@ -492,6 +569,8 @@ const OperatorStatusList = (): JSX.Element => {
       <Box display="flex" justifyContent="flex-end" style={{ marginTop: 10 }}>
         <Stack spacing={2}>
           <Pagination
+            // 计算表格中数据行数，然后将其除以默认每页展示的行数（DEFAULT_PER_PAGE_SIZE），得出需要展示的页数。
+            // 使用Math.ceil()函数对计算结果进行向上取整，得到页数。最后将页数作为属性传递给组件。
             count={Math.ceil(
               operator.operatorList.length / DEFAULT_PER_PAGE_SIZE
             )}
