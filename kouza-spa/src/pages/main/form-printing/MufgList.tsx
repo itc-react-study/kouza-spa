a82dbeae47d216d7565d1fb24c2581bcf094b50a
movanda <movanda@hotmail.co.jp>
2023-03-21 21:53:43 +0900

    add: mufg-list page

---
+++ kouza-spa/src/pages/main/form-printing/MufgList.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { MainContext } from "../../../store/store";
+
+/**
+ * MufgList
+ *
+ * @returns {JSX.Element}
+ */
+const MufgList = (): JSX.Element => {
+  return <h1>MufgList</h1>;
+};
+
+export default MufgList;
