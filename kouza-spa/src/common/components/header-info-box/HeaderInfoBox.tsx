f57a09ba53335eb9229ae6928e8404a419a9cd5f
CharlieYouu <99578012+CharlieYouu@users.noreply.github.com>
2023-03-25 04:05:11 +0900

    css ファイル整理

--- kouza-spa/src/common/components/header-info-box/HeaderInfoBox.tsx
+++ kouza-spa/src/common/components/header-info-box/HeaderInfoBox.tsx
@@ -10,43 +10,43 @@ const HeaderInfoBox = (): JSX.Element => {
     <div className="header-info-box">
       <div>
         <li>
-          <span className="fw">受付日</span>
+          <span className="header-info-box-title">受付日</span>
           <span>20210608</span>
         </li>
         <li>
-          <span className="fw">受付番号</span>
+          <span className="header-info-box-title">受付番号</span>
           <span>1608000091</span>
         </li>
         <li>
-          <span className="fw">支店号機名1</span>
+          <span className="header-info-box-title">支店号機名1</span>
           <span>新宿001</span>
         </li>
         <li>
-          <span className="fw">経過時間</span>
+          <span className="header-info-box-title">経過時間</span>
           <span>50分</span>
         </li>
         <li>
-          <span className="fw">ステータス</span>
+          <span className="header-info-box-title">ステータス</span>
           <span>山田：新宿</span>
         </li>
       </div>
 
       <div>
         <li>
-          <span className="fw">点検1</span>
+          <span className="header-info-box-title">点検1</span>
           <span>小林：新宿</span>
         </li>
         <li>
-          <span className="fw">ｵﾍﾟﾚｰｼｮﾝ</span>
+          <span className="header-info-box-title">ｵﾍﾟﾚｰｼｮﾝ</span>
           <span>小林：新宿</span>
         </li>
         <li>
-          <span className="fw">再鑑</span>
+          <span className="header-info-box-title">再鑑</span>
           <span>再鑑A：新宿</span>
         </li>
 
         <li>
-          <span className="fw">管理者確認1</span>
+          <span className="header-info-box-title">管理者確認1</span>
           <span>管理者：新宿</span>
         </li>
       </div>
