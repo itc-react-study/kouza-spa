4010ce144423bdb376d02daffd506a791013d289
CharlieYouu <99578012+CharlieYouu@users.noreply.github.com>
2023-04-08 07:38:50 +0900

    レイアウト修正

--- kouza-spa/src/common/components/identity-verification-doc-display/IdentityVerificationDocDisplay.tsx
+++ kouza-spa/src/common/components/identity-verification-doc-display/IdentityVerificationDocDisplay.tsx
@@ -6,18 +6,24 @@ import addIcon from "../../../assets/image/a-.svg";
 import minusIcon from "../../../assets/image/a--.svg";
 import styled from "@emotion/styled";
 import Button from "@mui/material/Button";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 
-const StyledButton = styled(Button)({
+const buttonStyle = {
   width: 60,
   height: 24,
   margin: "6px 0px",
-  fontSize: 13,
+  fontSize: 12,
   background: "#6083CB",
   "&:hover": {
     backgroundColor: "#40a9ff",
     color: "#fff",
   },
-});
+};
+
+const StyledButton = styled(Button)(buttonStyle);
+
+const AllButton = styled(Button)({ ...buttonStyle, width: 110 });
 
 const IdentityVerificationDocDisplay = () => {
   return (
@@ -46,7 +52,21 @@ const IdentityVerificationDocDisplay = () => {
         </div>
       </div>
 
-      <div className="identity-verification-doc-display-right"></div>
+      <div className="identity-verification-doc-display-right">
+        <div className="identity-verification-doc-display-right-all">
+          <AllButton variant="contained">全画面で展開</AllButton>
+        </div>
+        <div className="identity-verification-doc-display-right-update">
+          <StyledButton variant="contained">更 新</StyledButton>
+        </div>
+        <div className="identity-verification-doc-display-right-select">
+          <div>スキャンデータ</div>
+          <Select sx={{ width: "100%", height: 30 }}>
+            <MenuItem value={10}>免許証1</MenuItem>
+            <MenuItem value={20}>免許証2</MenuItem>
+          </Select>
+        </div>
+      </div>
     </div>
   );
 };
